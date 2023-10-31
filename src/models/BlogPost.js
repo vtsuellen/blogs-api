aa5696f92363d2962bccad5dffb0e4f3134bdd4b
vtsuellen <vtsuellen@hotmail.com>
2023-10-31 20:27:11 -0300

    refactor: Modelo de BlogPost e PostCategory

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -2,7 +2,11 @@ module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define(
     'BlogPost',
     {
-      id: DataTypes.INTEGER,
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       title: DataTypes.STRING(255),
       content: DataTypes.STRING(255),
       userId: DataTypes.INTEGER,
--- src/models/Category.js
+++ src/models/Category.js
@@ -1,6 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
-    id: DataTypes.INTEGER,
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: DataTypes.STRING,
   }, {
     tableName: 'user',
